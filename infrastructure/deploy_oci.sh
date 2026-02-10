6550921d09402cc9927c63b3c1d98263c139c08e
Michael Chow <michael.chow@alumni.stanford.edu>
2026-02-10 10:59:24 -0500

    feat(infra): expose postgres port 5432 and allow ingress in deployment script

--- infrastructure/deploy_oci.sh
+++ infrastructure/deploy_oci.sh
@@ -36,6 +36,7 @@ sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 443 -j ACCEPT
 sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 5050 -j ACCEPT
 sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 6080 -j ACCEPT
+sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 5432 -j ACCEPT
 sudo netfilter-persistent save
 
 # 4. Pull/Build Containers
